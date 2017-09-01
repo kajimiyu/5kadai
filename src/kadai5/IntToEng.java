fb436d964d2f0b2b91ca5959640be9e767562b82
g14916km <g14916km@s30406.ACTSUDA>
2017-09-01 11:57:01 +0900

    999まで対応しました

--- src/kadai5/IntToEng.java
+++ src/kadai5/IntToEng.java
@@ -11,31 +11,49 @@ package kadai5;
 			sc = new Scanner(System.in);
 			
 			
-			int input = sc.nextInt();
+			for(;;){
+				int input = sc.nextInt();
+			
 			
 			System.out.println(translateEng(input));
-			
+			}
 		}
 		
 		static String translateEng(int n){
 			String num[] = new String[]{"zero","one","two","three","four","five","six","seven","eight","nine","ten","eleven","twelve","thirteen","fourtenn","fifteen","sixteen","seventeen","eighteen","nineteen"};
 			String bignum[] = new String[]{"zero","one","twenty","thirty","fourty","fitty","sixty","seventy","eighty","ninety","one-handred"};
 			String input = null;
+			
+			
 			if(n < 20){
 				input = num[n];
 				
-			}else if(n >= 20 && n % 10 == 0){
+			}else if(n >= 20 && n % 10 == 0 && n < 99){
 				n = n / 10;
 				input = bignum[n];
 			
-			}else if(n >= 20 && n / 10 != 0){
+			}else if(n >= 20 && n / 10 != 0 && n <99){
 				int a = n % 10;
 				n = n / 10;
 				input = bignum[n] +  "-" + num[a];
 				
-			}else if(n > 100){
-				input = "sorry no data!";
+			
+			}else if(n >= 100 && n % 10 == 0 && n <1000){
+				int a = n / 100;
+				n = n - a * 100;
+				n = n / 10;
+				input = num[a] + "　handred　and " + bignum[n];
+			}else if((n >= 100 && n / 10 != 0 && n <1000)){
+				int  a = n / 100;
+				n = n - a * 100;
+				int b = n % 10;
+				n = n / 10;
+				
+				input = num[a] + "　handred　and " + bignum[n] + "-" + num[b];
+			}else{
+				input = "範囲外です";
 			}
+			
 			
 			return input;
 			
